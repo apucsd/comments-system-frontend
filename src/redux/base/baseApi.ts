2569f40798751b42d4fd0dada5e7fbce1508c8f9
apucsd <apusutradhar77@gmail.com>
2026-01-17 21:35:15 +0600

    added render deployed backend url

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -4,7 +4,7 @@ import type { RootState } from '../store';
 export const baseApi = createApi({
       reducerPath: 'baseApi',
       baseQuery: fetchBaseQuery({
-            baseUrl: 'http://localhost:5000/api/v1',
+            baseUrl: 'https://comments-system-server.onrender.com/api/v1',
 
             credentials: 'include',
             prepareHeaders: (header, { getState }) => {
